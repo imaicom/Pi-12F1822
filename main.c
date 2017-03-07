c5a3ee000bb03467a268bd3cf1f5acd09a600135
KURURU <i@okako.com>
2017-03-07 18:24:19 +0900

    1

--- main.c
+++ main.c
@@ -12,22 +12,26 @@ int main() {
 	fd = wiringPiI2CSetup(8); // I2Cのアドレス8番へ接続されているPIC 12F1822へアクセスする
 	
 	while(1) {
+		// Write,Read文にて書いてみた		
 		snd_data[0] = 0xA0; write(fd,snd_data,1); // RA0ポートのAN0を指定する
 		snd_data[0] = 0xAD; write(fd,snd_data,1); // AD変換を指定する
 		read(fd,rcv_data,2);
 		dat = (rcv_data[1] << 8) + rcv_data[0];
 		printf("%04x ",dat);
 
+		// wiringPiライブラリで書いてみた		
 		wiringPiI2CWrite(fd,0xA0); // RA0ポートのAN0を指定する
 		dat = wiringPiI2CReadReg16(fd,0xAD); // AD変換されたデータを読む
 		printf("%04x ",dat);
 		
+		// Write,Read文にて書いてみた		
 		snd_data[0] = 0xA4; write(fd,snd_data,1); // RA4ポートのAN3を指定する
 		snd_data[0] = 0xAD; write(fd,snd_data,1); // AD変換を指定する
 		read(fd,rcv_data,2);
 		dat = (rcv_data[1] << 8) + rcv_data[0];
 		printf("%04x ",dat);
 
+		// wiringPiライブラリで書いてみた		
 		wiringPiI2CWrite(fd,0xA4); // RA4ポートのAN3を指定する
 		dat = wiringPiI2CReadReg16(fd,0xAD); // AD変換されたデータを読む
 		printf("%04x\n",dat);
